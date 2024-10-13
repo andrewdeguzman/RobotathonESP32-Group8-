61bcfa8aaf48d8471378e3805a2f24bd6b15b457
Bryan <qazx1029@hotmail.com>
2024-10-13 18:32:01 -0500

    motor driver

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -26,6 +26,21 @@ limitations under the License.
 #include <ESP32SharpIR.h>
 #include <QTRSensors.h>
 
+Servo myservo;
+int pos = 0;
+
+int motor1Pin1 = 27;
+int motor1Pin2 = 26;
+int enable1Pin = 14;
+int motor2Pin1 = 25;
+int motor2Pin2 = 33;
+int enable2Pin = 32;
+
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int dutyCycle = 170;
+
 GamepadPtr myGamepads[BP32_MAX_GAMEPADS];
 
 // This callback gets called any time a new gamepad is connected.
@@ -63,6 +78,20 @@ void setup() {
 	ESP32PWM::allocateTimer(3);
 
     // TODO: Write your setup code here
+    //Serial.begin(115200);
+	//myservo.attach(13);
+
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(enable1Pin, OUTPUT);
+    pinMode(motor2Pin1, OUTPUT);
+    pinMode(motor2Pin2, OUTPUT);
+    pinMode(enable2Pin, OUTPUT);
+
+    ledcSetup(pwmChannel, freq, resolution);
+    ledcAttachPin(enable1Pin, pwmChannel);
+    ledcAttachPin(enable2Pin, pwmChannel);
+    ledcWrite(pwmChannel, dutyCycle);
 }
 
 // Arduino loop function. Runs in CPU 1
@@ -78,6 +107,47 @@ void loop() {
     }
 
     // TODO: Write your periodic code here
+    // Move the DC motor forward at maximum speed
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, HIGH);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, HIGH);
+    delay(2000);
+
+    // Stop the DC motor
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, LOW);
+    delay(1000);
+
+    // Move DC motor backwards at maximum speed
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW); 
+    delay(2000);
+
+    // Stop the DC motor
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, LOW);
+    delay(1000);
+
+    // Move DC motor forward with increasing speed
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW);
+    while (dutyCycle <= 255){
+        ledcWrite(enable1Pin, dutyCycle);
+        ledcWrite(enable2Pin, dutyCycle);   
+        dutyCycle = dutyCycle + 5;
+        delay(500);
+    }
+    dutyCycle = 170;
+    ledcWrite(pwmChannel, dutyCycle);
 
     vTaskDelay(1);
 }
